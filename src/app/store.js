a6061bd80f76d0d3abed186160fd8814ba05f111
Abhay <26abhaykumarsingh@gmail.com>
2023-12-14 16:50:38 +0530

    Connect Checkout and add orders

--- src/app/store.js
+++ src/app/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import productReducer from "../features/product/ProductSlice";
 import authReducer from "../features/auth/authSlice";
 import cartReducer from "../features/cart/cartSlice";
+import orderReduxer from "../features/order/orderSlice";
 
 export const store = configureStore({
   reducer: {
     product: productReducer,
     auth: authReducer,
     cart: cartReducer,
+    order: orderReduxer,
   },
 });
